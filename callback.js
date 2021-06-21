339a031f2da36a889e4e3472889153a2b7927840
adambrockhaus <adam.m.brockhaus@gmail.com>
2021-06-21 08:01:13 -0500

    Added a timeout to the calculations

--- callback.js
+++ callback.js
@@ -4,27 +4,33 @@ let calcButton = document.getElementById("calculate").addEventListener("click", 
 
 function addition(num1, num2, showAns){
     let sum = num1 + num2;
-    showAns(sum);
+    total.innerHTML = "Thinking...."
+    setTimeout(showAns, 2000, sum);
 }
 
 function subtraction(num1, num2, showAns){
     let diff = num1 - num2;
-    showAns(diff);
+    total.innerHTML = "Thinking...."
+    setTimeout(showAns, 2000, diff);
 }
 
 function multiplication(num1, num2, showAns){
     let prod = num1 * num2;
-    showAns(prod);
+    total.innerHTML = "Thinking...."
+    setTimeout(showAns, 2000, prod);
 }
 
 function division(num1, num2, showAns){
     let quot = num1 / num2;
-    showAns(quot);
+    total.innerHTML = "Thinking...."
+    setTimeout(showAns, 2000, quot);
 }
 
 function modulus(num1, num2, showAns){
     let modulus = num1 % num2;
-    showAns(modulus);
+    total.innerHTML = "Thinking...."
+    setTimeout(showAns, 2000, modulus);
+    
 }
 
 function showAns(answer){
@@ -32,11 +38,17 @@ function showAns(answer){
     total.innerHTML = "Total: " + answer;
 }
 
+function thinking(){
+    
+
+}
+
 function calculate(){
     operation = document.getElementById("operation").value;
     num1 = Number(document.getElementById("num1").value);
     num2 = Number(document.getElementById("num2").value);
     console.log(operation);
+    
     if (operation == "Addition"){
         addition(num1, num2, showAns);
     }
